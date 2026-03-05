10996861c5b75bcd4d2a57f614104754114ca84f
LavanyaPoojary02 <lavanyapoojary39@gmail.com>
2026-03-05 17:52:22 +0530

    Create Substraction.java

---
+++ Substraction.java
@@ -0,0 +1,9 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 40;
+        int result = a - b;
+
+        System.out.println("Subtraction = " + result);
+    }
+}
