b9eedfc38314bd8debbfd28ad51fd3f7368e506e
LavanyaPoojary02 <lavanyapoojary39@gmail.com>
2026-03-11 18:31:19 +0530

    Create Simple Calculators.py

---
+++ Simple Calculators.py
@@ -0,0 +1,20 @@
+# Program for Addition, Subtraction, Multiplication and Division
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Addition
+addition = a + b
+print("Addition:", addition)
+
+# Subtraction
+subtraction = a - b
+print("Subtraction:", subtraction)
+
+# Multiplication
+multiplication = a * b
+print("Multiplication:",multiplication )
+
+# Division
+division = a / b
+print("Division:", division)
