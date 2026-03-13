5ec7d3baef0b09e4aa66afe4e41d011c57b0198f
LavanyaPoojary02 <lavanyapoojary39@gmail.com>
2026-03-13 21:15:48 +0530

    Create basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+a=50
+b=20
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+print("Addition of two numbers:", addition)
+print("Subtraction of two numbers:", subtraction)
+print("Multiplication of two numbers:", multiplication)
+print("Division of two numbers:", division)
