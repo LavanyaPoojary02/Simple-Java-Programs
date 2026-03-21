67cc472a67c7ba4925c8eae5121262ef9e3129e1
LavanyaPoojary02 <lavanyapoojary39@gmail.com>
2026-03-21 21:04:43 +0530

    Create reverse_string.py

---
+++ reverse_string.py
@@ -0,0 +1,10 @@
+text="Helloo"
+
+# To Reverse the String
+reversed_string=text[::-1]
+
+# Original String
+print ("***Original String is***:",text)
+
+#Reversed String 
+print("***Reversed String is***:",reversed_string)
