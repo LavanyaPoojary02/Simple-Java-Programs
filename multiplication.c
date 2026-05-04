97f5014cba4d304229c920ac930e5645cdae3f21
LavanyaPoojary02 <lavanyapoojary39@gmail.com>
2026-05-04 06:48:58 +0530

    Create multiplication.c

---
+++ multiplication.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, multiplication;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    multiplication = num1 * num2;
+
+    printf("Sum = %d", sums);
+
+    return 0;
+}
